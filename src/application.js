088f832fd8e565f68f515247c33948f8f4b1635e
Timursus <redundant09@gmail.com>
2020-06-19 13:46:27 +0300

    move i18n resources to separate file

--- src/application.js
+++ src/application.js
@@ -1,5 +1,6 @@
 import { string } from 'yup';
 import i18next from 'i18next';
+import resources from './locales';
 import { loadNewChannel, refresh } from './contentLoad';
 import render from './view';
 
@@ -50,30 +51,7 @@ export default () => {
   i18next.init({
     lng: 'en',
     debug: true,
-    resources: {
-      en: {
-        translation: {
-          loading: {
-            success: 'RSS successfully loaded',
-            parsingError: 'Sorry, failed to process. Probably, it is not RSS link.',
-            networkError: {
-              timeout: 'Network problems. Please, try again.',
-              status3xx: 'Redirection network error. Please check your URL.',
-              status4xx: 'Client network error. Please check your URL.',
-              status5xx: 'Server error. Please, try again later.',
-            },
-          },
-          validationErrors: {
-            notValidURL: 'Please enter a valid URL',
-            alreadyAdded: 'This RSS has already been added',
-          },
-          addButton: {
-            default: 'Add',
-            loading: ' Loading...',
-          },
-        },
-      },
-    },
+    resources,
   }).then((t) => render(state, elements, t));
 
   elements.urlInput.addEventListener('input', (e) => {
---
+++ src/locales/en.js
@@ -0,0 +1,22 @@
+export default {
+  translation: {
+    loading: {
+      success: 'RSS successfully loaded',
+      parsingError: 'Sorry, failed to process. Probably, it is not RSS link.',
+      networkError: {
+        timeout: 'Network problems. Please, try again.',
+        status3xx: 'Redirection network error. Please check your URL.',
+        status4xx: 'Client network error. Please check your URL.',
+        status5xx: 'Server error. Please, try again later.',
+      },
+    },
+    validationErrors: {
+      notValidURL: 'Please enter a valid URL',
+      alreadyAdded: 'This RSS has already been added',
+    },
+    addButton: {
+      default: 'Add',
+      loading: ' Loading...',
+    },
+  },
+};
