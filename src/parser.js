c7d6538141436bc0a17f7f12134cba417043238d
Timursus <redundant09@gmail.com>
2020-11-30 14:29:11 +0300

    refactor function with optional chaining operator

--- src/parser.js
+++ src/parser.js
@@ -1,28 +1,27 @@
 export default (rss) => {
   const domparser = new DOMParser();
   const doc = domparser.parseFromString(rss, 'text/xml');
+
   const feedTitle = doc.querySelector('channel>title').textContent;
-  const feedDescription = doc.querySelector('channel>description').textContent;
-  const feed = {
-    feedTitle, feedDescription,
-  };
-  const image = doc.querySelector('channel>image>url');
-  if (image) {
-    feed.imageUrl = image.textContent;
-  }
+  const feedDescription = doc.querySelector('channel>description')?.textContent ?? '';
+  const imageUrl = doc.querySelector('channel>image>url')?.textContent ?? null;
+  const feed = { feedTitle, feedDescription, imageUrl };
+
   const postsItems = doc.querySelectorAll('item');
   const feedPosts = [...postsItems].map((item) => {
     const title = item.querySelector('title').textContent;
     const link = item.querySelector('link').textContent;
     const date = new Date(item.querySelector('pubDate').textContent);
+    const description = item.querySelector('description')?.textContent ?? '';
     const post = {
-      title, link, date, feedTitle,
+      title,
+      link,
+      date,
+      description,
+      feedTitle,
     };
-    const descriptionNode = item.querySelector('description');
-    if (descriptionNode) {
-      post.description = descriptionNode.textContent;
-    }
     return post;
   });
+
   return { feed, feedPosts };
 };
